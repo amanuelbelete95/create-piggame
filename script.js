3dccb75bc0a902380968375e2a7266cdcfbe1b8f
Amanuel Belete <amanuelbelete95@gmail.com>
2023-10-10 16:29:05 +0300

    Set the basic setup of the game

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+//
+const score1 = document.querySelector('#score--0');
+const score2 = document.querySelector('#score--1');
+
+const diceEl = document.querySelector('.dice');
+
+score1.textContent = 0;
+score2.textContent = 0;
+diceEl.classList.add('hidden');

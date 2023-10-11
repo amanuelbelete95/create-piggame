dc719c10339123471bea71599da318edf230a509
Amanuel Belete <amanuelbelete95@gmail.com>
2023-10-11 11:25:05 +0300

    Switch the background color of the active-player

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 //
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score1 = document.querySelector('#score--0');
 const score2 = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -49,7 +52,14 @@ const setDice = function () {
   } else {
     // if we role dice = 1; we loose our score and switch the player
     //switch the  players
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    // document.querySelector('.player').classList.add('active--player');
+    currentScore = 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 };
 

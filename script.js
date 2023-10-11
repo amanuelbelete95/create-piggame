752b895874c1720558cf6a37584c19780c085353
Amanuel Belete <amanuelbelete95@gmail.com>
2023-10-11 10:48:55 +0300

    Switch the active player based on the score of the dice

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ const current1El = document.querySelector('#current--1');
 
 const diceEl = document.querySelector('.dice');
 
+// select the button for evenlistener functionality
 const btnRoll = document.querySelector('.btn--roll');
-
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 
 //current score
 // we don't want to store the data on user interface or dom;
+
+const scores = [0, 0];
 let currentScore = 0;
-console.log(currentScore, typeof currentScore);
+let activePlayer = 0;
 
 score1.textContent = 0;
 score2.textContent = 0;
@@ -33,40 +35,21 @@ const setDice = function () {
   //remove hidden classes from dice;
 
   diceEl.classList.remove('hidden');
-
   //display dice
   diceEl.src = `dice-${dice}.png`;
-  /*switch (dice) {
-    case 1:
-      diceEl.src = 'dice-1.png';
-      break;
 
-    case 2:
-      diceEl.src = 'dice-2.png';
-      break;
-    case 3:
-      diceEl.src = 'dice-3.png';
-      break;
-    case 4:
-      diceEl.src = 'dice-4.png';
-      break;
-    case 5:
-      diceEl.src = 'dice-5.png';
-      break;
-    case 6:
-      diceEl.src = 'dice-6.png';
-      break;
-    default:
-      diceEl.src = '';
-  }
-  */
   // check for if one is rolled
   if (dice !== 1) {
     //add the score to the current score of the player
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    // current0El.textContent = currentScore; // change later
+    // the dice score should be displayed under the active player when the click event happend
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // if we role dice = 1; we loose our score and switch the player
     //switch the  players
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 };
 
